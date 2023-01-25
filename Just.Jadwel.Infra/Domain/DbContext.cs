fbb15c88830eda349f7fc88a90fc63414ace81c8
DEIA'A ALSHAWISH <daalshawish20@cit.just.edu.jo>
2023-01-25 11:34:32 +0300

    Create DB Connection

--- Just.Jadwel.Infra/Domain/DbContext.cs
+++ Just.Jadwel.Infra/Domain/DbContext.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Just.Jadwel.Core.Domain;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
 
 namespace Just.Jadwel.Infra.Domain
 {
-    class DbContext
+    public class DbContext : IDbContext
     {
+        private DbConnection connection;
+        private IConfiguration configuration;
+
+        public DbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public DbConnection dbConnection
+        {
+            get
+            {
+                if (connection == null)
+                {
+                    connection = new OracleConnection(configuration["ConnectionStrings:DBConnectionString"]);
+                    connection.Open();
+                }
+                else if (connection.State != System.Data.ConnectionState.Open)
+                    connection.Open();
+
+                return connection;
+            }
+        }
     }
 }
